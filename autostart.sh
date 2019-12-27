30c8299161872c3f915acaaaee64768c7ef7499a
YongDing.Zhou <yondy.chow@gmail.com>
2019-12-27 21:53:43 +0800

    screen resolution

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-xrandr --auto --output HDMI1 --same-as eDP1 --size 1920x1080
+# xrandr --auto --output DP1 --same-as eDP1 --size 1920x1080
+xrandr --output eDP1 --primary --mode 1920x1080
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
-/bin/bash ~/scripts/setxmodmap-colemak.sh &
 nm-applet &
 xfce4-power-manager &
 xfce4-volumed-pulse &
